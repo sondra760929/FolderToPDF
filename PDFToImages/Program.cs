aefb6ad334525c3e7a861414b84af0fa372be466
sondra76 <sondra76@gmail.com>
2019-03-13 11:37:54 +0900

    기존 방식으로 변경. 메모리 사용을 위해 64전용으로

--- PDFToImages/Program.cs
+++ PDFToImages/Program.cs
@@ -17,6 +17,8 @@ namespace PDFToImages
     class Program
     {
         private static string exe_file_name;
+        private static int current_console_line = 0;
+
         static void Main(string[] args)
         {
             if (args.Length > 0)
@@ -28,6 +30,59 @@ namespace PDFToImages
 
         static void PDFToImages(string path)
         {
+            DirectoryInfo d = new DirectoryInfo(path);
+
+            DirectoryInfo[] dirs = d.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                PDFToImages(dir.FullName);
+            }
+            FileInfo[] files = d.GetFiles();
+            if (files.Length > 0)
+            {
+                foreach (FileInfo file in files)
+                {
+                    if (file.Extension.ToLower() == ".pdf")
+                    {
+                        string current_folder = path + "\\" + Path.GetFileNameWithoutExtension(file.Name);
+                        DirectoryInfo di = new DirectoryInfo(current_folder);
+                        int RenderDPI = 300;
+                        if (di.Exists == false)
+                        {
+                            di.Create();
+                        }
+                        MuPDF _mupdf;
+                        try
+                        {
+                            _mupdf = new MuPDF(file.FullName, null);
+                            _mupdf.AntiAlias = true;
+                            Console.Write("Convert PDF : " + file.FullName + "\n");
+                            current_console_line++;
+                            for (int i = 1; i <= _mupdf.PageCount; i++)
+                            {
+                                _mupdf.Page = i;
+                                Bitmap FiratImage = _mupdf.GetBitmap(0, 0, RenderDPI, RenderDPI, 0, RenderType.RGB, false, false, 100000000);
+                                if (FiratImage != null)
+                                {
+                                    FiratImage.Save(String.Format("{0}\\{1:D4}.jpg", current_folder, i));
+                                }
+                                Console.SetCursorPosition(0, current_console_line);
+                                Console.Write(i.ToString() + " / " + _mupdf.PageCount.ToString());
+                            }
+                            Console.Write("\n");
+                            current_console_line++;
+                        }
+                        catch (Exception e)
+                        {
+                            //throw new Pdf2KTException("Error while opening PDF document.", e);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void PDFToImages2(string path)
+        {
             string ext = path.Substring(path.Length - 4, 4).ToLower();
             if (ext == ".pdf")
             {
@@ -36,7 +91,7 @@ namespace PDFToImages
                 //string current_folder = path + "\\" + Path.GetFileNameWithoutExtension(file.Name);
                 string current_folder = path.Substring(0, path.Length - 4);
                 DirectoryInfo di = new DirectoryInfo(current_folder);
-                int RenderDPI = 96;
+                int RenderDPI = 300;
                 if (di.Exists == false)
                 {
                     di.Create();
@@ -115,7 +170,8 @@ namespace PDFToImages
                     {
                         if (file.Extension.ToLower() == ".pdf")
                         {
-                            Process.Start(@exe_file_name, "\"" + file.FullName + "\"");
+                            PDFToImages(file.FullName);
+                            //Process.Start(@exe_file_name, "\"" + file.FullName + "\"");
                         }
                     }
                 }
